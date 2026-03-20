3b0d752aff18f979a88bbab6165c8103d54ee631
Ethan Snow <ethansnowballs@proton.me>
2026-03-20 12:04:13 -0500

    Initial steamlit app creation.

---
+++ app/app.py
@@ -0,0 +1,6 @@
+import streamlit
+
+streamlit.set_page_config(page_title = "Night Owl Real Estate Pricing Tool")
+
+streamlit.title("Night Owl Real Estate Pricing Tool")
+streamlit.subheader("Prototype dashboard for home price recommendations")
