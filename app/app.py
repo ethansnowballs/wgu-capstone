89f6c7328c47fbc98201703a12d8b5edd3c61fec
Ethan Snow <ethansnowballs@proton.me>
2026-03-20 13:14:18 -0500

    Created histogram chart and removed initial data that was displayed. Added comments

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 import streamlit
+import plotly.express
 from sklearn.datasets import fetch_california_housing
 
 streamlit.set_page_config(page_title = "Night Owl Real Estate Pricing Tool")
@@ -6,9 +7,21 @@ streamlit.set_page_config(page_title = "Night Owl Real Estate Pricing Tool")
 streamlit.title("Night Owl Real Estate Pricing Tool")
 streamlit.subheader("Prototype dashboard for home price recommendations")
 
+# load dataset
 housing = fetch_california_housing(as_frame = True)
+# pandas table
 hf = housing.frame
 
-streamlit.write("Rows:", len(hf))
-streamlit.write("Columns:", len(hf.columns))
-streamlit.dataframe(hf.head(25), use_container_width = True)
+# create histogram chart
+histogram_chart = plotly.express.histogram(
+    hf,
+    x = "MedHouseVal",
+    color_discrete_sequence = ["#B22234"],
+    nbins = 40,
+    title = "Distribution of Median House Value",
+)
+histogram_chart.update_xaxes(title_text = "Median House Value")
+histogram_chart.update_yaxes(title_text = "Count")
+
+# display histogram chart
+streamlit.plotly_chart(histogram_chart)
