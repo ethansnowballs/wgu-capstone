8286e36f97a927ac226f9b9928da3e94b09b6e7e
Ethan Snow <ethansnowballs@proton.me>
2026-04-03 19:45:18 -0500

    Created scatter plot and further formatted histogram chart

--- app/app.py
+++ app/app.py
@@ -3,7 +3,6 @@ import plotly.express
 from sklearn.datasets import fetch_california_housing
 
 streamlit.set_page_config(page_title = "Night Owl Real Estate Pricing Tool")
-
 streamlit.title("Night Owl Real Estate Pricing Tool")
 streamlit.subheader("Prototype dashboard for home price recommendations")
 
@@ -39,6 +38,7 @@ streamlit.sidebar.info(
 # copy filtered dataframe for histogram dollar column
 filtered_hf = filtered_hf.copy()
 filtered_hf["MedHouseValDollars"] = filtered_hf["MedHouseVal"] * 100_000
+filtered_hf["MedIncDollars"] = filtered_hf["MedInc"] * 10_000
 
 streamlit.info(
     "This chart shows the distribution of median home values for areas within the selected median income range."
@@ -52,10 +52,38 @@ histogram_chart = plotly.express.histogram(
     nbins = 20,
     title = "Distribution of Median House Value",
 )
+
+# format histogram chart
 histogram_chart.update_xaxes(title_text = "Median House Value")
 histogram_chart.update_yaxes(title_text = "Count")
 histogram_chart.update_traces(
+    marker_line_color = "#3C3B6E",
+    marker_line_width = 1.5,
     hovertemplate = "Median House Value: $%{x:,.0f}<br>Count: %{y}<extra></extra>"
 )
+
 # display histogram chart
 streamlit.plotly_chart(histogram_chart)
+
+streamlit.info(
+    "Each point shows one area. This chart compares median income to median house value."
+)
+
+# create scatter plot chart
+scatter_chart = plotly.express.scatter(
+    filtered_hf,
+    x = "MedIncDollars",
+    y = "MedHouseValDollars",
+    title="Median Income vs Median House Value"
+)
+
+# format scatter plot chart
+scatter_chart.update_xaxes(title_text = "Median Income")
+scatter_chart.update_yaxes(title_text = "Median House Value")
+scatter_chart.update_traces(
+    marker = dict(color = "white", line = dict(color = "black", width = 1)),
+    hovertemplate = "Median Income: $%{x:,.0f}<br>Median House Value: $%{y:,.0f}<extra></extra>"
+)
+
+# display scatter chart
+streamlit.plotly_chart(scatter_chart)
